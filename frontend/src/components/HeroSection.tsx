050310d79deb063309978619d8d2dd19b9fd5570
common-cold <prajjwalkohli@gamil.com>
2025-04-14 22:32:05 +0000

    Added hero section

---
+++ frontend/src/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+export function HeroSection() {
+    return <div style={{display: "flex", flexDirection: "column", width: "350px", alignItems: "center"}}>
+        <div className="black" style={{fontFamily: "Satoshi-Black", fontSize: "45px", marginBottom: "10px"}}>
+            Bridge-kun 🌉
+        </div>
+        <div style={{fontFamily: "Satoshi-Regular", fontSize: "20px", marginBottom: "10px", textAlign: "center"}}>
+            Experience fast, secure, and intuitive transfers across blockchain networks.
+        </div>
+    </div>
+}
